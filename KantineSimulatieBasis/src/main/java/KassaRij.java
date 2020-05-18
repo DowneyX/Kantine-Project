d3797403bae8418385a548cc4818d25c6823a36f
downey <dutch.x@outlook.com>
2020-05-18 15:53:17 +0200

    MODIFEID: added functionality to the Kassarij Class

--- KantineSimulatieBasis/src/main/java/KassaRij.java
+++ KantineSimulatieBasis/src/main/java/KassaRij.java
@@ -2,12 +2,13 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 
 public class KassaRij {
+    ArrayList<Dienblad> rij;
 
     /**
      * Constructor
      */
     public KassaRij() {
-        // method body omitted
+        rij = new ArrayList<Dienblad>();
     }
 
     /**
@@ -16,17 +17,23 @@ public class KassaRij {
      * @param klant
      */
     public void sluitAchteraan(Dienblad klant) {
-        // method body omitted
+        rij.add(klant);
     }
 
     /**
-     * Indien er een rij bestaat, de eerste klant uit de rij verwijderen en retourneren. Als er
-     * niemand in de rij staat geeft deze null terug.
+     * Indien er een rij bestaat, de eerste klant uit de rij verwijderen en
+     * retourneren. Als er niemand in de rij staat geeft deze null terug.
      *
      * @return Eerste klant in de rij of null
      */
     public Dienblad eerstePersoonInRij() {
-        // method body omitted
+        if (!erIsEenRij()) {
+            return null;
+        } else {
+            Dienblad eerstepersoon = rij.get(0);
+            rij.remove(0);
+            return eerstepersoon;
+        }
     }
 
     /**
@@ -35,6 +42,10 @@ public class KassaRij {
      * @return Of er wel of geen rij bestaat
      */
     public boolean erIsEenRij() {
-        // method body omitted
+        if (rij.size() == 0) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
